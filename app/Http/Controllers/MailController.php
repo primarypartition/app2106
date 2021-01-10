b854e71bd49ae42e40fae7c1f609e481c2c7b2ed
Your Name <you@example.com>
2021-01-10 16:10:06 -0700

    mail

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,48 +3,65 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;
-use App\Department;
+
+use App\Models\User;
+
 use App\Mail\SendMail;
+
 class MailController extends Controller
 {
-    public function create(){
+    /**
+     *
+     */
+    public function create()
+    {
     	return view('admin.mail.create');
     }
-    public function store(Request $request){
+
+    /**
+     *
+     */
+    public function store(Request $request)
+    {
  		$this->validate($request,[
  			'file'=>'mimes:docx,doc,pdf,jpeg,jpg,png',
  			'body'=>'required'
-
  		]);
- 		
- 		$image = $request->file('file');
+
+        $image = $request->file('file');
+
  		$details = [
  			'body'=>$request->body,
  			'file'=>$image
- 		];
- 		if($request->department){
- 			$users = User::where('department_id',$request->department)->get();
- 			foreach($users as $user){
- 				
- 				\Mail::to($user->email)
- 				->send(new SendMail($details));
- 			
+        ];
 
- 			}
+        if($request->department)
+        {
+             $users = User::where('department_id', $request->department)
+                          ->get();
+
+ 			foreach($users as $user) {
+ 				\Mail::to($user->email)
+ 				     ->send(new SendMail($details));
+             }
+
  		}elseif($request->person){
- 			$user = User::where('id',$request->person)->first();
- 			$userEmail=$user->email;
- 			\Mail::to($user->email)
- 				->send(new SendMail($details));
+             $user = User::where('id', $request->person)
+                         ->first();
 
+            $userEmail = $user->email;
+
+ 			\Mail::to($userEmail)
+ 				 ->send(new SendMail($details));
  		}else{
- 			$users = User::get();
+             $users = User::get();
+
  			foreach($users as $user){
  				\Mail::to($user->email)
  				->send(new SendMail($details));
  			}
- 		}
- 		return redirect()->back()->with('message','Email sent');
+         }
+
+ 		return redirect()->back()->with('message', 'Email sent');
     }
 }
